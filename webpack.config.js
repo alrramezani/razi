4c4ec8469a162701b3e3d90fedbebdc75719bac0
Alireza Ramezani <alireza@Alirezas-MacBook-Pro.local>
2021-01-20 15:12:05 +0330

    config inital webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(js)$/, 
-                use: 'babel-loader' 
-            }
-        ]
-    },
-    mode: 'development',
-    plugins: [
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
-    ]
-}
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
